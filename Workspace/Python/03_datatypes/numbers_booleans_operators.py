218d05963f1423d1b81b40ac7b9c401d8d087010
Shivay Kapoor <shivaydigital.com@gmail.com>
2026-06-06 14:38:37 +0530

    solved the second video of 3rd section which is about data types.

---
+++ Workspace/Python/03_datatypes/numbers_booleans_operators.py
@@ -0,0 +1,30 @@
+# This file is of (15. Numbers, Booleans and Operators in Depth in Python)
+
+# print(3 + 4)
+# print(3 - 4)
+# print(3 * 4)
+# print(3 / 4)
+# print(3 // 4)
+# print(3 % 4)
+# print(3 ** 4)
+
+# Writing Large Numbers
+income_this_year = 1_000_000_00
+print(income_this_year)
+
+# Booleans
+is_boiling = True # 1
+is_money_credited = False # 2
+
+# Logical Operators
+tea = True
+coffee = False
+biscuit = True
+
+print(coffee or biscuit)
+
+# Real Numbers (Precision)
+chandigarh_temp = 35.45
+yamunanagar_temp = 30.8
+
+print(f"Difference {round(chandigarh_temp - yamunanagar_temp, 2)}")
