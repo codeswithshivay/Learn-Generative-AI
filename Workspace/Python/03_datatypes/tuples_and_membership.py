978c405c1dd07e4e11cdf3d69125892c12c41ae0
Shivay Kapoor <shivaydigital.com@gmail.com>
2026-06-09 14:33:05 +0530

    watched the 17th video of 3rd section. the video was related to tuples.

---
+++ Workspace/Python/03_datatypes/tuples_and_membership.py
@@ -0,0 +1,13 @@
+# Video (Play 17. Tuples and Membership Testing)
+
+masala_spices = ("cardamom", "cloves", "salt")
+
+(spice1, spice2, spice3) = masala_spices
+
+a = 2, 1
+
+# Membership
+
+print(f'is salt in masala spices ? {'salt' in masala_spices}')
+print(f'is salt not in masala spices ? {'salt' in masala_spices}')
+print(f'is salt not in masala spices ? {not ('salt' in masala_spices)}')
