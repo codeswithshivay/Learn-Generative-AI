0f01320663394089666ff3c6197fe012433c4a27
Shivay Kapoor <shivaydigital.com@gmail.com>
2026-06-10 14:38:53 +0530

    watched the 19th video of 3rd section of data types.

---
+++ Workspace/Python/03_datatypes/operator_overloading_and_bytearray.py
@@ -0,0 +1,10 @@
+# Video Play 19. Operator overloading and bytearray in python
+
+# base_ingredients = ['sugar', 'salt']
+# extra_ingredients = ['cardamom', 'chilli']
+
+# print(base_ingredients + extra_ingredients)
+
+# Trying Special Method __add__
+
+print((1).__add__(2))
