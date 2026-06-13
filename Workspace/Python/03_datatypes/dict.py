878d07d5e43bfa041b822ec93b585e45a7f688bf
Shivay Kapoor <shivaydigital.com@gmail.com>
2026-06-13 15:28:51 +0530

    watched the 21st Video of 3rd section.

---
+++ Workspace/Python/03_datatypes/dict.py
@@ -0,0 +1,14 @@
+# a = ["Shivay", "Kapoor"]
+# print(a[1])
+
+user = {
+  "first_name": "Shivay",
+  "last_name": "Kapoor",
+  "bio_data":{
+    "age": 21,
+    "education": "High School"
+  }
+}
+
+print("First Name: ", user.get("first_nam", 0))
+print("bio_data" in user)
