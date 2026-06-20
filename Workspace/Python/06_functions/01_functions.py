a2d28e9ff093962a5da6eaf790f004ac88e1978a
Shivay Kapoor <shivaydigital.com@gmail.com>
2026-06-20 14:10:47 +0530

    watched the 39th video of the 6th section

---
+++ Workspace/Python/06_functions/01_functions.py
@@ -0,0 +1,26 @@
+# Video 39
+
+# First Problem
+# def print_order(customer, chai_type):
+#     print(f'Customer: {customer} ordered - {chai_type} chai')
+
+# print_order('Raju', 'Cardamom`')
+
+# Second Problem
+
+def fetch_sales():
+    print('Fetching the sales')
+
+def filter_valid_orders():
+    print('Filtering the valid orders')
+
+def summarize_data():
+    print('Summerizing the data')
+
+def generate_report():
+    fetch_sales()
+    filter_valid_orders()
+    summarize_data()
+    print('Report generated!')
+
+generate_report()
