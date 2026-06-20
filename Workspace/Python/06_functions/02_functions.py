f5eb67a12628cd0a90852d45f43067346b9b839a
Shivay Kapoor <shivaydigital.com@gmail.com>
2026-06-20 14:43:23 +0530

    watched the 40th video of 6th section

---
+++ Workspace/Python/06_functions/02_functions.py
@@ -0,0 +1,33 @@
+# Video 40
+
+# First Challange
+# def get_input():
+#     print('Getting the input')
+
+# def validate_input():
+#     print('Validating the input')
+
+# def save_to_db():
+#     print('Saving the user to db')
+
+# def register_user():
+#     get_input()
+#     validate_input()
+#     save_to_db()
+#     print('User registered!')
+
+# register_user()
+
+# Second Challange
+
+# def calculate_bill(cups, price_per_cup):
+#     return cups * price_per_cup
+
+# print('1st Bill', calculate_bill(2, 10))
+
+# Third Challange
+
+# def calculate_vat(price, vat):
+#     return price + (price * vat / 100)
+
+# print('Vat', calculate_vat(100, 10))
