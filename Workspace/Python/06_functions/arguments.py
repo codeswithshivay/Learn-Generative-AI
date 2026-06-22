43571e718dce1828a5e907443a11c7a114f67a80
Shivay Kapoor <shivaydigital.com@gmail.com>
2026-06-22 15:24:30 +0530

    watched the 43rd video of 6th section

---
+++ Workspace/Python/06_functions/arguments.py
@@ -0,0 +1,7 @@
+# Video 43
+
+def test(*positional_arguments, **keyword_arguments):
+    print('Positional Arguments', positional_arguments)
+    print('Keyword Arguments', keyword_arguments)
+
+test('Shivay', 16, location="Delhi, India", address="Mohan Nagar, House number 11")
