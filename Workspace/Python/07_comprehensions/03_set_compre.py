49f396d9dc85651ed2331065db9d7af92b575934
Shivay Kapoor <shivaydigital.com@gmail.com>
2026-06-24 15:49:52 +0530

    watched the 50th video of 7th section

---
+++ Workspace/Python/07_comprehensions/03_set_compre.py
@@ -0,0 +1,14 @@
+# Video 50
+
+# favourite_chais = ['Ginger Tea', 'Masala Tea', 'Cardamom Tea', 'Masala Tea']
+# unique_chai = {chai for chai in favourite_chais if not 'Ginger' in chai}
+# print(unique_chai)
+
+# recipes = {
+#     'Masala Chai': ['ginger', 'cardamom', 'clove'],
+#     'Elaichi Chai': ['cardamom', 'milk'],
+#     'Spicy Chai': ['ginger', 'black pepper', 'clove']
+# }
+
+# unique_spices = [ingredients for ingredients in recipes.values() {spice} for ]
+# print(unique_spices)
