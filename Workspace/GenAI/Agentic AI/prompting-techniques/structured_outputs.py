364acfb043fe16f2d8e2992a4ac32918fa0e9795
Shivay Kapoor <shivaydigital.com@gmail.com>
2026-08-08 14:48:59 +0530

    Watched the 119th video of the gen ai course.

---
+++ Workspace/GenAI/Agentic AI/prompting-techniques/structured_outputs.py
@@ -0,0 +1,66 @@
+# Few-shot prompting:
+# A technique where the Large Language Model (LLM) is provided with one or more 
+# examples (shots) demonstrating the desired behavior, format, or style of the response.
+# This guides the model to learn the pattern and apply it to the new user request.
+
+
+# Imports
+# pyrefly: ignore [missing-import]
+from dotenv import load_dotenv
+import os
+# pyrefly: ignore [missing-import]
+import openai
+
+# Load environment variables
+dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
+load_dotenv(dotenv_path)
+
+# Initialize OpenAI
+client = openai.OpenAI(
+   api_key=os.getenv("GEMINI_API_KEY"),
+   base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+SYSTEM_PROMPT = \
+"""
+  You are an assistant that helps the user with programming.
+  Your name is India AI.
+  You avoid helping the user for non-programming questions.
+  You only help user in programming.
+  You can help with python, java, c++, javascript and any other programming language.
+  You can help with code, 
+
+  Some examples:
+
+  Q: What is 5 + 2?
+  A: {"is_applicable": false, "response": ""}
+
+  Q: What is the capital of India?
+  A: {"is_applicable": false, "response": ""}
+
+  Q: Generate a javascript function that can modify an DOM element
+  A: {"is_applicable": true, "response": "function modifyDOM() {\n  const element = document.getElementById('myElement');\n  if (element) {\n    element.textContent = 'Modified DOM element';\n    element.style.color = 'red';\n  }\n}"}
+
+  Output Format:
+  1. You have to respond in JSON format.
+  2. The JSON object should have two keys: "is_applicable" (boolean) and "response" (string).
+  3. If the user's question is related to programming, set "is_applicable" to true and "response" to the answer.
+  4. If the user's question is not related to programming, set "is_applicable" to false and "response" to an empty string.
+  5. If "is_applicable" is false, set "response" to an empty string.
+  6. If "is_applicable" is true, set "response" to the answer.
+"""
+
+USER_PROMPT = "What's your name?"
+
+# Request
+response = client.chat.completions.create(
+   model="gemini-3.5-flash-lite",
+   messages=[
+      { "role": "system", "content": SYSTEM_PROMPT },
+      { "role": "user", "content": USER_PROMPT }
+   ]
+)
+
+# Response message
+response_message = response.choices[0].message
+print(response_message.content)
